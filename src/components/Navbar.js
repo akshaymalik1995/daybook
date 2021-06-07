f534e5322ef3480e38c5f1126bf216416f9985f2
akshaymalik1995 <akshaymalik191@gmail.com>
2021-06-07 22:13:47 +0530

    Navbar Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,10 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand navbar-light bg-light">
   
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+  
+  
     <ul class="navbar-nav">
       <li class="nav-item active">
         <Link className="nav-link" to="">Home</Link>
@@ -17,7 +15,7 @@ export const Navbar = () => {
       </li>
       
     </ul>
-  </div>
+  
 </nav>
     )
 }
